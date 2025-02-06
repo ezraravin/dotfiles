91d2f9dff06408a7db900232eb22294f75371540
Ezra <Ezra Ravin Mateus>
2025-02-06 18:45:37 +0700

    keymaps for window management change

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -5,7 +5,7 @@ vim.g.maplocalleader = " "
 local opts = { noremap = true, silent = true }
 
 -- Window Management Splits
-vim.keymap.set("n", "<leader>s\\", "<C-w>v", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>s|", "<C-w>v", { desc = "Split window horizontally" })
 vim.keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
@@ -23,8 +23,8 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Navigate to Top Window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Navigate to Right Window" })
 
 -- Create New Window Pane TMUX
-vim.keymap.set("n", "<leader>ts\\", "<cmd>silent !tmux split-window -h<cr>", { desc = "New Window Pane Horizontal" })
-vim.keymap.set("n", "<leader>ts-", "<cmd>silent !tmux split-window -v<cr>", { desc = "New Window Pane Vertical" })
+vim.keymap.set("n", "<leader>t|", "<cmd>silent !tmux split-window -h<cr>", { desc = "New Window Pane Horizontal" })
+vim.keymap.set("n", "<leader>t-", "<cmd>silent !tmux split-window -v<cr>", { desc = "New Window Pane Vertical" })
 
 -- Clear Highlight
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear Search Highlights" })
