d10a98d676a13eac7e1875a682f83a0194b22840
Rave's Endeavour <ezraravin@proton.me>
2025-03-24 22:59:07 +0700

    linux

--- .scripts/endeavour/setup_linux.sh
+++ .scripts/endeavour/setup_linux.sh
@@ -15,4 +15,14 @@ sudo pacman -S --noconfirm hyprland hyprpaper zsh neovim tmux eza waybar obs-stu
 # INSTALL JETBRAINS NERD FONT
 # curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip && sudo unzip JetBrainsMono.zip -d /usr/share/fonts/truetype/ && sudo rm -rf JetBrainsMono.zip
 #
-sudo usermod -a -G uucp $USER
+sudo usermod -a -G uucp $USER # Try this first (common alternative)
+sudo usermod -a -G lock $USER # Some systems use this
+sudo usermod -a -G tty $USER  # Another common alternative
+
+sudo groupadd dialout
+sudo usermod -a -G dialout $USER
+
+sudo nano /etc/udev/rules.d/99-esp32.rules
+SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE="0666"
+sudo udevadm control --reload-rules
+sudo udevadm trigger
