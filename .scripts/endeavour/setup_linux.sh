514ddcb7df29e28d9fcd610ac174af6f8c83828a
Rave <ezraravin@proton.me>
2025-03-25 10:08:52 +0000

    Update 2 files
    - /.scripts/endeavour/steps.md
    - /.scripts/endeavour/setup_linux.sh

--- .scripts/endeavour/setup_linux.sh
+++ .scripts/endeavour/setup_linux.sh
@@ -4,7 +4,7 @@
 ssh-keygen -t ed25519 -C "Rave's PC"
 
 # Install Hyprland
-sudo pacman -S --noconfirm hyprland hyprpaper zsh neovim tmux eza waybar obs-studio cava fastfetch yazi lazygit zoxide zsh-syntax-highlighting zsh-autosuggestions yarn fzf eza yazi lazygit thefuck wl-clipboard pavucontrol vlc blueman network-manager-applet networkmanager bat ffmpeg wofi btop nautilus
+sudo pacman -S --noconfirm hyprland hyprpaper zsh neovim tmux eza waybar obs-studio cava fastfetch yazi lazygit zoxide zsh-syntax-highlighting zsh-autosuggestions yarn fzf eza yazi lazygit thefuck wl-clipboard vlc blueman bat wofi btop nautilus
 
 # MARP CLI
 # yay -S --noconfirm marp-cli spotify spotify-adblock whatsapp-for-linux arduino-ide xampp android-studio android-sdk android-sdk-build-tools android-sdk-cmdline-tools-latest chatbox-bin
--- .scripts/endeavour/steps.md
+++ .scripts/endeavour/steps.md
@@ -1,2 +1,3 @@
 1. Install Drivers
 2. Install Desktop Manager
+3. Install Packages
