62a2e0650ae0c141ed94446939df9f34483f9f70
Rave <ezraravin@proton.me>
2025-03-25 09:52:48 +0000

    Edit setup_nvidia.sh

--- .scripts/endeavour/setup_nvidia.sh
+++ .scripts/endeavour/setup_nvidia.sh
@@ -1,2 +1,2 @@
 # Install NVIDIA Drivers
-  install_or_skip "nvidia" 'sudo pacman -S --noconfirm nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings' "Installing NVIDIA Drivers"
+  install_or_skip "nvidia" 'sudo pacman -S --noconfirm nvidia-lts nvidia-utils lib32-nvidia-utils nvidia-settings' "Installing NVIDIA Drivers"
