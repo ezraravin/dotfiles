119a9bb1104841b81ae02f78e365e25ac91f9244
Rave <ezraravin@proton.me>
2025-03-27 22:18:26 +0700

    fuck you

--- .scripts/endeavour/base_install.sh
+++ .scripts/endeavour/base_install.sh
@@ -4,7 +4,7 @@
 # SSH Setup
 read -p "Use SSH for Git? [y/N]: " ssh_choice
 if [[ "$ssh_choice" =~ ^[Yy]$ ]]; then
-  ssh-keygen -t ed25519 -C "$(hostname) $(date +%Y-%m-%d)" -N "" -f ~/.ssh/id_ed25519
+  ssh-keygen -t ed25519 -C "$(hostname)" -N "" -f ~/.ssh/id_ed25519
   eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519
   echo "Public key (add to Git account):"
   cat ~/.ssh/id_ed25519.pub
@@ -14,10 +14,12 @@ else
 fi
 
 # Dotfiles
+echo "Configuring Dotfiles"
 [ ! -d ~/dotfiles ] && git clone $GIT_CLONE_PREFIX/ezraravinmateus/dotfiles.git ~/dotfiles &&
   cp -r ~/dotfiles/. ~/ && rm -rf ~/dotfiles
 
 # Git Config
+echo "Configuring Git"
 if ! git config --global user.email &>/dev/null; then
   read -p "Git email: " git_email
   read -p "Git name: " git_name
@@ -27,8 +29,13 @@ if ! git config --global user.email &>/dev/null; then
 fi
 
 # System
+echo "Enabling Bluetooth"
 sudo systemctl enable --now bluetooth
 
-# Reboot
-read -p "Reboot now? [y/N]: " reboot_choice
+# Quick System Finalization
+echo "Updating system..."
+sudo pacman -Syu --noconfirm
+echo "Cleaning up packages..."
+sudo pacman -Sc --noconfirm
+read -p "Setup Complete. Reboot now? [y/N]: " reboot_choice
 [[ "$reboot_choice" =~ ^[Yy]$ ]] && sudo reboot
