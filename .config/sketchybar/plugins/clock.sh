bb9263dc88792132afd3499a710cc5ac4e9e566d
Rave <ezraravin@proton.me>
2025-03-28 20:34:34 +0700

    bye sketchybar

--- .config/sketchybar/plugins/clock.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sketchybar --set clock label="$(date +'%H:%M')" label.color=$COLOR_FOREGROUND
--- .config/sketchybar/plugins/front_app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-front_app=$(osascript -e 'tell application "System Events" to get name of first application process whose frontmost is true')
-sketchybar --set front_app label="$front_app" label.color=$COLOR_FOREGROUND
