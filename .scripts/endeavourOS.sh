66a74e15705a88fcab5e652c1c52900c75b92302
Rave <ezraravin@proton.me>
2025-03-28 09:43:43 +0000

    Update file endeavourOS.sh

--- .scripts/endeavourOS.sh
+++ .scripts/endeavourOS.sh
@@ -66,7 +66,7 @@ echo "✅ GPU setup complete"
 
 # Core Tools
 echo "🐧 Core Setup"
-sudo pacman -S --noconfirm eza bat ripgrep fd nautilus btop cava fastfetch blueman
+sudo pacman -S --noconfirm eza bat ripgrep fd nautilus btop cava fastfetch blueman kitty
 echo "✅ Core tools installed"
 
 # Window Managers
