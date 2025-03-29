9b12ac239a261edc1a3966e3209362df31ae7ccb
Rave <ezraravin@proton.me>
2025-03-29 10:01:31 +0000

    Update file macOS.sh

--- .scripts/macOS.sh
+++
@@ -1,9 +0,0 @@
-##############################################
-### Shell Environment Setup
-##############################################
-setup_shell_environment() {
-  echo "🐚 Configuring Shell Environment..."
-
-  # PDF Support for Markdown to PDF
-  brew install basictex pandoc
-}
