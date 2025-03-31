f786701a7d370c985b46c3e1037e11a179636b62
Rave <ezraravin@proton.me>
2025-04-01 06:25:53 +0700

    hm

--- .scripts/endeavour/ai.sh
+++ .scripts/endeavour/ai.sh
@@ -3,5 +3,5 @@
 # echo "export DEEPSEEK_API_KEY=\"$key\"" >>~/.zshenv && source ~/.zshenv
 curl -fsSL https://ollama.com/install.sh | sh
 ollama pull deepseek-r1:1.5b
-docker pull ghcr.io/open-webui/open-webui:main
+
 docker run -d -p 3000:8080 -v open-webui:/app/backend/data --name open-webui ghcr.io/open-webui/open-webui:main
