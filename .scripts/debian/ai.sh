72f64fbe21e7f7c447f65a23de95cac22f3bc5b2
Rave <ezraravin@proton.me>
2025-04-01 05:31:27 +0700

    hm

---
+++ .scripts/debian/ai.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt get -y curl
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull deepseek-r1:1.5b
+curl -fsSL https://get.casaos.io | sudo bash
--- .scripts/endeavour/ai.sh
+++ .scripts/endeavour/ai.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 read -p "Enter Deepseek API key: " key
 echo "export DEEPSEEK_API_KEY=\"$key\"" >>~/.zshenv && source ~/.zshenv
-yay -S --noconfirm chatbox-bin
