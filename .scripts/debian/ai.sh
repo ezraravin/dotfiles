e21f5414c831f39c02d313915259147ca1d86f49
Rave <ezraravin@proton.me>
2025-04-01 07:06:36 +0700

    ollama

--- .scripts/debian/ai.sh
+++ .scripts/debian/ai.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt install -y curl
 curl -fsSL https://ollama.com/install.sh | sh
-ollama pull deepseek-r1:1.5b
 curl -fsSL https://get.casaos.io | sudo bash
+ollama pull deepseek-r1:1.5b
+docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
