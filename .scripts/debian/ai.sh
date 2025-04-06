e167d7ec24e8c71ad441b4de20b8bb791d434a4c
Rave <ezraravin@proton.me>
2025-04-06 22:28:52 +0000

    Edit ai.sh

--- .scripts/debian/ai.sh
+++ .scripts/debian/ai.sh
@@ -10,11 +10,6 @@ ollama pull deepseek-r1
 # Open Web UI
 docker run -d --network=host -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 
-# OPEN WEB UI with NVIDIA
-```bash
-docker run -d --network=host --gpus all -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
-```
-
 # Bolt AI Coder
 git clone -b stable https://github.com/stackblitz-labs/bolt.diy.git
 cd bolt.diy && pnpm install && pnpm run dev --open --host
