9a63229986fffb8dc7bce2f8daf8fccc873384a6
Rave <ezraravin@proton.me>
2025-04-11 02:51:00 +0700

    podman

--- .scripts/debian/install.sh
+++ .scripts/debian/install.sh
@@ -14,17 +14,8 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
-# 🐳 Docker
-# Add Docker's official GPG key:
-sudo apt-get update && sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# 🦭 Podman
+sudo apt install podman
 
 # 🛠️ CORE SETUP
 echo "🌟 Core Tools Installation"
