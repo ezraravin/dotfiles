409f0877186ff9b2755dcd2b1a0c5f1de178be38
Rave <ezraravin@proton.me>
2025-04-11 22:59:42 +0700

    try this

--- .scripts/debian/install.sh
+++ .scripts/debian/install.sh
@@ -11,8 +11,7 @@ done 2>/dev/null &
 
 # 🦭 Podman & Podman Compose
 sudo apt install -y podman pipx cmake git curl wget bat ripgrep zsh zsh-syntax-highlighting zsh-autosuggestions zoxide fzf ninja-build gettext
-pipx ensurepath
-pipx install podman-compose
+sh -c "$(curl -sSL https://raw.githubusercontent.com/containers/podman-compose/main/scripts/download_and_build_podman-compose.sh)"
 podman-compose --version
 systemctl enable --now podman.socket
 
