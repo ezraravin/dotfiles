580fa155d3494e92dadfedc20a2f47c3ad2ab052
Rave <ezraravin@proton.me>
2025-04-12 01:37:20 +0700

    mm

--- .scripts/debian/install.sh
+++ .scripts/debian/install.sh
@@ -10,7 +10,9 @@ while true; do
 done 2>/dev/null &
 
 # 🦭 Podman & Podman Compose
-sudo apt install -y podman pipx cmake git curl wget bat ripgrep zsh zsh-syntax-highlighting zsh-autosuggestions zoxide fzf ninja-build gettext
+sudo apt install -y podman pipx make cmake git curl wget bat ripgrep zsh zsh-syntax-highlighting zsh-autosuggestions zoxide fzf ninja-build gettext
+sudo pipx install podman-compose
+sudo systemctl enable --now podman.socket
 
 # 🛠️ CORE SETUP
 echo "🌟 Core Tools Installation"
@@ -33,6 +35,7 @@ sudo chsh -s $(which zsh) $USER
 echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >>.zshrc
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>.zshrc
 echo "export DOCKER_HOST=unix:///run/podman/podman.sock" >>.zshrc
+sudo pipx ensurepath
 
 # 🔄 REBOOT
 echo "🎉 Setup complete! Rebooting in 5 seconds..."
