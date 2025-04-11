3ba4d19ba9a51f0a1315d09d6332e9abb2c7252a
Rave <ezraravin@proton.me>
2025-04-11 17:33:40 +0700

    uhh

--- .scripts/mac/install.sh
+++ .scripts/mac/install.sh
@@ -53,9 +53,7 @@ killall SystemUIServer Dock Finder
 ##############################################
 
 # Install Homebrew if needed
-if ! command -v brew >/dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Configure Homebrew
 if [ -f "/opt/homebrew/bin/brew" ]; then
