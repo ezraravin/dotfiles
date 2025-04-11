74e2b378b0102abd3b746aa5ede73a57369e1e8f
Rave <ezraravin@proton.me>
2025-04-11 17:31:51 +0700

    hmm

--- .scripts/debian/install.sh
+++ .scripts/debian/install.sh
@@ -11,7 +11,7 @@ done 2>/dev/null &
 
 # 🦭 Podman & Podman Compose
 sudo apt install -y podman pipx cmake git curl wget bat ripgrep zsh zsh-syntax-highlighting zsh-autosuggestions zoxide fzf ninja-build gettext
-pipx ensurepath # Adds ~/.local/bin to your PATH
+pipx ensurepath
 pipx install podman-compose
 podman-compose --version
 
