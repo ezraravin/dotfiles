e18a7a7a8e10339f8fe0a4af7da9b271eea747db
Rave <ezraravin@proton.me>
2025-04-13 22:46:23 +0700

    cloudflare

--- .scripts/endeavour/install.sh
+++ .scripts/endeavour/install.sh
@@ -84,7 +84,10 @@ echo "👨💻 Dev Setup"
 sudo pacman -S --noconfirm nodejs npm python yarn pnpm lazygit docker docker-compose visidata
 sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
-yay -S --noconfirm lazydocker
+yay -S --noconfirm lazydocker cloudflare-warp-bin
+sudo systemctl start warp-svc
+sudo systemctl enable warp-svc
+warp-cli registration new
 /bin/bash -c "$(curl -fsSL https://php.new/install/linux)"
 curl -fsSL https://bun.sh/install | bash
 echo "✅ Dev tools installed"
