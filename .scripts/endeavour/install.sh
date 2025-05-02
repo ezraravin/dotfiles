07fd8c7eb18f258d9e3f0a644b04328df7890d84
Rave <ezraravin@proton.me>
2025-05-02 19:02:22 +0700

    mysql workbench

--- .scripts/endeavour/install.sh
+++ .scripts/endeavour/install.sh
@@ -97,7 +97,7 @@ echo "✅ Dev tools installed"
 
 # Shell
 echo "🐚 Shell Setup"
-sudo pacman -S --noconfirm zsh zsh-syntax-highlighting zsh-autosuggestions zsh-completions tmux neovim zoxide fzf thefuck imagemagick librsvg chafa ffmpeg ttf-jetbrains-mono-nerd ttf-jetbrains-mono yazi
+sudo pacman -S --noconfirm zsh zsh-syntax-highlighting zsh-autosuggestions zsh-completions tmux neovim zoxide fzf thefuck imagemagick librsvg chafa ffmpeg ttf-jetbrains-mono-nerd ttf-jetbrains-mono yazi mysql-workbench
 curl -s https://ohmyposh.dev/install.sh | bash -s
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
