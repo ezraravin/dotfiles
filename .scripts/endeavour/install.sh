ec9087565502be7602f79fb2f0808b9e41a01286
ezraravin <ezraravin@proton.me>
2025-09-14 23:04:58 +0700

    a

--- .scripts/endeavour/install.sh
+++ .scripts/endeavour/install.sh
@@ -131,14 +131,6 @@ echo "💾 Ventoy"
 yay -S --noconfirm ventoy-bin
 echo "✅ Apps installed"
 
-# Arduino
-echo "⚡ Arduino Setup"
-yay -S --noconfirm arduino-ide
-sudo usermod -a -G uucp,tty $USER
-sudo curl -o /etc/udev/rules.d/60-arduino.rules https://raw.githubusercontent.com/arduino/ArduinoCore-avr/master/60-arduino-avr-core.rules
-sudo udevadm control --reload
-echo "✅ Arduino ready"
-
 # Final update
 sudo pacman -Syu --noconfirm
 sudo pacman -Sc --noconfirm
